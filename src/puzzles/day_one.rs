db09c7f1f2a6f44717f79c2e3d24adbe0d9cc5f9
J-Me-2310 <jamie@blaettlers.ch>
2024-12-01 22:29:44 +0100

    Solve day 1, part two

--- src/puzzles/day_one.rs
+++ src/puzzles/day_one.rs
@@ -30,17 +30,37 @@ pub fn run_day_one() {
     right_numbers.sort();
 
     let solution_part_one = part_one(&left_numbers, &right_numbers);
-    println!("The solution for day 1, part a is {}", solution_part_one)
+    println!("The solution for day 1, part a is {}", solution_part_one);
+    let solution_part_two = part_two(&left_numbers, &right_numbers);
+    println!("The solution for day 1, part b is {}", solution_part_two);
 }
 
-fn part_one(left: &Vec<i32>, right: &Vec<i32>) -> i32{
+fn part_one(left: &Vec<i32>, right: &Vec<i32>) -> i32 {
     let mut difference = 0;
 
     for i in 0..left.len() {
         difference += (left[i] - right[i]).abs();
     }
-    
+
     difference
+}
+
+fn part_two(left: &Vec<i32>, right: &Vec<i32>) -> i32 {
+    let mut similarity_score = 0;
+
+    for i in left {
+        let mut count = 0;
+
+        for j in right {
+            if i == j{
+                count += 1;
+            }
+        }
+
+        similarity_score += count * i;
+    }
+
+    similarity_score
 }
 
 #[cfg(test)]
@@ -50,11 +70,11 @@ mod test {
     #[test]
     fn part_one_test() {
         // Arrange
-       let mut left = vec![3, 4, 2, 1, 3, 3];
+        let mut left = vec![3, 4, 2, 1, 3, 3];
         left.sort();
         let mut right = vec![4, 3, 5, 3, 9, 3];
         right.sort();
-        
+
         let expected = 11;
 
         // Act
@@ -62,5 +82,22 @@ mod test {
 
         // Assert
         assert_eq!(expected, actual);
+    }
+
+    #[test]
+    fn part_two_test() {
+        // Arrange
+        let mut left = vec![3, 4, 2, 1, 3, 3];
+        left.sort();
+        let mut right = vec![4, 3, 5, 3, 9, 3];
+        right.sort();
+
+        let expected = 31;
+
+        // Act
+        let actual = part_two(&left, &right);
+
+        // Assert
+        assert_eq!(expected, actual)
     }
 }
