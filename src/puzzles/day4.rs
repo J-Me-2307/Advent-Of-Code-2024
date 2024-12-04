6bdf76aa593cbf7cec0694488bff20dc74fca9b8
J-Me-2310 <jamie@blaettlers.ch>
2024-12-04 22:13:07 +0100

    Solve day 4, part b

--- src/puzzles/day4.rs
+++ src/puzzles/day4.rs
@@ -1,6 +1,7 @@
 use std::{
     fs::File,
     io::{self, BufRead, BufReader},
+    isize,
 };
 
 use crate::file_helper::get_file;
@@ -16,6 +17,9 @@ pub fn run() {
 
     let solution_part_one = part_one(&grid);
     println!("The solution for day 4, part a is {}", solution_part_one);
+
+    let solution_part_two = part_two(&grid);
+    println!("The solution for day 4, part b is {}", solution_part_two);
 }
 
 fn parse_input(reader: BufReader<File>) -> Vec<Vec<char>> {
@@ -28,8 +32,8 @@ fn parse_input(reader: BufReader<File>) -> Vec<Vec<char>> {
     grid
 }
 
-fn get_dirs() -> Vec<(isize, isize)> {
-    vec![
+fn part_one(grid: &Vec<Vec<char>>) -> i32 {
+    let directions = vec![
         (0, 1),   // Horizontal right
         (0, -1),  // Horizontal left
         (1, 0),   // Vertical down
@@ -38,11 +42,7 @@ fn get_dirs() -> Vec<(isize, isize)> {
         (1, -1),  // Diagonal down-left
         (-1, 1),  // Diagonal up-right
         (-1, -1), // Diagonal up-left
-    ]
-}
-
-fn part_one(grid: &Vec<Vec<char>>) -> i32 {
-    let directions = get_dirs();
+    ];
 
     let mut count = 0;
     for row in 0..grid.len() {
@@ -79,6 +79,43 @@ fn search(
     true
 }
 
+fn part_two(grid: &Vec<Vec<char>>) -> i32 {
+    let mut count = 0;
+    let rows = grid.len();
+    let cols = grid[0].len();
+    for row in 1..rows - 1 {
+        for col in 1..cols - 1 {
+            if row >= 1 && row < grid.len() - 1 && col >= 1 && col < grid[row].len() - 1 {
+                if grid[row][col] == 'A' && is_x(&grid, (row, col)) {
+                    count += 1;
+                }
+            }
+        }
+    }
+    count
+}
+
+fn is_x(grid: &[Vec<char>], (row, col): (usize, usize)) -> bool {
+    let top_left = grid[row - 1][col - 1];
+    let top_right = grid[row - 1][col + 1];
+    let bottom_left = grid[row + 1][col - 1];
+    let bottom_right = grid[row + 1][col + 1];
+
+    // Check if corners are valid
+    if !matches!(top_left, 'M' | 'S')
+        || !matches!(top_right, 'M' | 'S')
+        || !matches!(bottom_left, 'M' | 'S')
+        || !matches!(bottom_right, 'M' | 'S')
+    {
+        return false;
+    }
+
+    let diagonal1 = format!("{}{}{}", top_left, grid[row][col], bottom_right);
+    let diagonal2 = format!("{}{}{}", top_right, grid[row][col], bottom_left);
+
+    (diagonal1 == "SAM" || diagonal1 == "MAS") && (diagonal2 == "SAM" || diagonal2 == "MAS")
+}
+
 #[cfg(test)]
 mod test {
     use super::*;
@@ -102,6 +139,29 @@ mod test {
 
         // Act
         let actual = part_one(&input);
+
+        // Assert
+        assert_eq!(expected, actual);
+    }
+
+    #[test]
+    fn part_two_test() {
+        let input: Vec<Vec<char>> = vec![
+            "MMMSXXMASM".chars().collect::<Vec<char>>(),
+            "MSAMXMSMSA".chars().collect::<Vec<char>>(),
+            "AMXSXMAAMM".chars().collect::<Vec<char>>(),
+            "MSAMASMSMX".chars().collect::<Vec<char>>(),
+            "XMASAMXAMM".chars().collect::<Vec<char>>(),
+            "XXAMMXXAMA".chars().collect::<Vec<char>>(),
+            "SMSMSASXSS".chars().collect::<Vec<char>>(),
+            "SAXAMASAAA".chars().collect::<Vec<char>>(),
+            "MAMMMXMMMM".chars().collect::<Vec<char>>(),
+            "MXMXAXMASX".chars().collect::<Vec<char>>(),
+        ];
+        let expected = 9;
+
+        // Act
+        let actual = part_two(&input);
 
         // Assert
         assert_eq!(expected, actual);
